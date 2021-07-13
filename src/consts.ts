bf2e349b1d6f00341835b3faa6f3942361124754
Sunho Kim <ksunhokim123@naver.com>
2021-07-14 01:45:29 +0900

    Update

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
-export const DEFAULT_INDEX_AMOUNT = 1000.0;
-export const MARKET_UPDATE_MS = 1000;
+export const DEFAULT_INDEX_AMOUNT = 5000;
+export const MARKET_UPDATE_MS = 60*1000;
 export const BOT_UPDATE_MS = 100;
 export const BOT_REQ_EXPIRE_MS = 30*60*1000;
 export const BOT_AMOUNT_VAR = 10;
